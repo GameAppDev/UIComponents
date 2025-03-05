8f05470663f804ba630e2c8d3f8512cb1cea86e4
vphq\oguzhan.yalcin <oguzhan.yalcin@alternatifbank.com.tr>
2025-03-06 02:07:01 +0300

    package init

--- UIComponents/Package.swift
+++ UIComponents/Package.swift
@@ -1,24 +1,26 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "UIComponents",
-    platforms: [.iOS(.v12)],
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         .library(
             name: "UIComponents",
             targets: ["UIComponents"]
         ),
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "UIComponents",
-            dependencies: [],
             resources: [
                 .process("Resources")
             ]
-        )
+        ),
     ]
 )
