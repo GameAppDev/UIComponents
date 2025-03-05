ff1dcc9029be54a31b331e3ddc06d848c6ef6d88
vphq\oguzhan.yalcin <oguzhan.yalcin@alternatifbank.com.tr>
2025-03-06 02:22:23 +0300

    code updated

--- Sources/UIComponents/InfoView.swift
+++ Sources/UIComponents/InfoView.swift
@@ -14,49 +14,29 @@ public final class InfoView: UIView {
     @IBOutlet private weak var infoImageView: BaseImageView!
     @IBOutlet private weak var infoLabel: BaseLabel!
     
-    // MARK: - Init
-     public override init(frame: CGRect) {
-         super.init(frame: frame)
-         commonInit()
-     }
-     
-     public required init?(coder: NSCoder) {
-         super.init(coder: coder)
-         commonInit()
-     }
-     
-     private func commonInit() {
-         // Bundle.module ile InfoView.xib dosyasını yükle
-         let bundle = Bundle.module
-         bundle.loadNibNamed("InfoView", owner: self, options: nil)
-         
-         addSubview(containerView)
-         containerView.frame = bounds
-         
-         // Auto Layout Constraints
-         containerView.translatesAutoresizingMaskIntoConstraints = false
-         NSLayoutConstraint.activate([
-             containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
-             containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
-             containerView.topAnchor.constraint(equalTo: topAnchor),
-             containerView.bottomAnchor.constraint(equalTo: bottomAnchor)
-         ])
-     }
-     
-     // MARK: - Override Methods
-     public override func layoutSubviews() {
-         super.layoutSubviews()
-         setupView()
-     }
-     
-     // MARK: - Methods
-     private func setupView() {
-         // Özel düzenlemeler yapılabilir.
-     }
-     
-     public func configureViews(imageViewModel: ImageViewModel, labelModel: LabelModel) {
-         // ImageView ve Label'ı yapılandırıyoruz.
-         infoImageView.configure(with: imageViewModel)
-         infoLabel.configure(with: labelModel)
-     }
- }
+    // MARK: Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)  // Bu satır çok önemli!
+        commonInit()  // commonInit metodunu burada çağırıyoruz
+    }
+    
+    private func commonInit() {
+        // XIB dosyasını yüklemek için
+        Bundle(for: InfoView.self).loadNibNamed("InfoView", owner: self, options: nil)
+        addSubview(containerView)
+        containerView.frame = bounds
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}
