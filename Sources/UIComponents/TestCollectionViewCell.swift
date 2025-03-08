9fadaa82741b8352d60acfa999cca2fe1cc9a618
vphq\oguzhan.yalcin <oguzhan.yalcin@alternatifbank.com.tr>
2025-03-08 21:59:31 +0300

    try

---
+++ Sources/UIComponents/TestCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TestCollectionViewCell.swift
+//  UIComponents
+//
+//  Created by OguzhanYalcin on 8.03.2025.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
