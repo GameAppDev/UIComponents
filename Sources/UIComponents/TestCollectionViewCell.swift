f6922594620de09b33595f3ee9c7bb6f901e4c06
vphq\oguzhan.yalcin <oguzhan.yalcin@alternatifbank.com.tr>
2025-03-08 22:32:27 +0300

    try

--- Sources/UIComponents/TestCollectionViewCell.swift
+++ Sources/UIComponents/TestCollectionViewCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/*
 public class TestCollectionViewCell: UICollectionViewCell {
 
     public override func awakeFromNib() {
@@ -16,5 +17,17 @@ public class TestCollectionViewCell: UICollectionViewCell {
     
     public static func getNib() -> UINib {
         return UINib(nibName: "TestCollectionViewCell", bundle: Bundle.module)
+    }
+}
+*/
+public class TestCollectionViewCell: UICollectionViewCell {
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    public static func getNib() -> UINib {
+        // Bundle.for kullanılarak doğru bundle'a referans verildiğinden emin olun
+        return UINib(nibName: "TestCollectionViewCell", bundle: Bundle(for: TestCollectionViewCell.self))
     }
 }
